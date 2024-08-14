52fb7da49dbc00327f5f716fdd83c682f34d393d
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2024-08-14 14:32:02 +0800

    sidebar admin and verifikator

--- resources/views/layouts/dashboard/partials/sidebar.blade.php
+++ resources/views/layouts/dashboard/partials/sidebar.blade.php
@@ -12,13 +12,109 @@
         <ul class="menu">
             <li class="sidebar-title">Menu</li>
 
-            <li class="sidebar-item {{ (request()->routeIs('dashboard*') || auth()->user()->is_public) && !request()->routeIs('profile*') ? 'active' : '' }}">
+            <li
+                class="sidebar-item {{ (request()->routeIs('dashboard*') || auth()->user()->is_public) && !request()->routeIs('profile*') ? 'active' : '' }}">
                 <a class="sidebar-link"
                    href="{{ route('dashboard') }}">
                     <i class="isax isax-element-4"></i>
                     <span>Dashboard</span>
                 </a>
             </li>
+
+            @if (auth()->user()->is_admin)
+                <li
+                    class="sidebar-item actives">
+                    <a class="sidebar-link"
+                       href="#">
+                        <i class="isax isax-receipt"></i>
+                        <span>Permohonan</span>
+                    </a>
+                </li>
+                <li class="sidebar-item has-sub actives">
+                    <a class="sidebar-link"
+                       href="#">
+                        <i class="isax isax-folder-2"></i>
+                        <span>Master Data</span>
+                    </a>
+                    <ul class="submenu">
+                        <li class="submenu-item actives">
+                            <a class="submenu-link"
+                               href="#">
+                                User</a>
+                        </li>
+                        <li class="submenu-item actives">
+                            <a class="submenu-link"
+                               href="#">
+                                Jenis Ijin</a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="sidebar-item has-sub actives">
+                    <a class="sidebar-link"
+                       href="#">
+                        <i class="isax isax-task-square"></i>
+                        <span>Laporan</span>
+                    </a>
+                    <ul class="submenu">
+                        <li class="submenu-item actives">
+                            <a class="submenu-link"
+                               href="#">
+                                Ijin Terbit Bulanan</a>
+                        </li>
+                        <li class="submenu-item actives">
+                            <a class="submenu-link"
+                               href="#">
+                                Rekap Permohonan</a>
+                        </li>
+                        <li class="submenu-item actives">
+                            <a class="submenu-link"
+                               href="#">
+                                Survey Layanan</a>
+                        </li>
+                    </ul>
+                </li>
+                <li
+                    class="sidebar-item actives">
+                    <a class="sidebar-link"
+                       href="#">
+                        <i class="isax isax-radar"></i>
+                        <span>Log Aktivitas</span>
+                    </a>
+                </li>
+            @endif
+
+            @if (auth()->user()->is_verifikator)
+                <li
+                    class="sidebar-item actives">
+                    <a class="sidebar-link"
+                       href="#">
+                        <i class="isax isax-receipt"></i>
+                        <span>Verifikasi</span>
+                    </a>
+                </li>
+                <li class="sidebar-item has-sub actives">
+                    <a class="sidebar-link"
+                       href="#">
+                        <i class="isax isax-task-square"></i>
+                        <span>Laporan</span>
+                    </a>
+                    <ul class="submenu">
+                        <li class="submenu-item actives">
+                            <a class="submenu-link"
+                               href="#">
+                                Detail Permohonan</a>
+                        </li>
+                    </ul>
+                </li>
+                <li
+                    class="sidebar-item actives">
+                    <a class="sidebar-link"
+                       href="#">
+                        <i class="isax isax-document-1"></i>
+                        <span>Surat Permohonan</span>
+                    </a>
+                </li>
+            @endif
         </ul>
     </div>
 </div>
