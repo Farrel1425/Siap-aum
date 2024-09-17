64d36322a78c1ca23b990994feb386cc6ab5aac4
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2024-09-17 15:42:32 +0800

    test

--- resources/views/layouts/landing/main-base.blade.php
+++ resources/views/layouts/landing/main-base.blade.php
@@ -11,6 +11,9 @@
     @vite(['resources/sass/app.scss'])
     <link href="{{ asset('assets/vendors/iconsax/style.css') }}"
           rel="stylesheet">
+    <link href="{{ asset('assets/images/logo.png') }}"
+          rel="shortcut icon"
+          type="image/x-icon">
 </head>
 
 <body id="app">
@@ -18,7 +21,8 @@
         <div class="loader"></div>
     </div>
     <div class="min-vh-100 d-flex flex-column">
-        <div class="{{ request()->routeIs('user-guide', 'check-application') ? 'position-static' : 'position-absolute' }} d-none d-lg-block" style="z-index: 999;">
+        <div class="{{ request()->routeIs('user-guide', 'check-application') ? 'position-static' : 'position-absolute' }} d-none d-lg-block"
+             style="z-index: 999;">
             @include('layouts.landing.partials.navbar-dekstop')
         </div>
         @include('layouts.landing.partials.navbar-mobile')
