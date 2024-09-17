add731e0caf06e2befd7b027898c9187e8a791f7
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2024-09-17 13:59:56 +0800

    test

--- resources/views/components/landing/hero.blade.php
+++ resources/views/components/landing/hero.blade.php
@@ -3,7 +3,7 @@
     'backButtonLabel' => null,
 ])
 
-<section class="position-relative hero row g-0" style="backgound-image: url('{{ asset('assets/images/main-bg.svg') }}')">
+<section class="position-relative hero row g-0" style="background-image: url('{{ asset('assets/images/main-bg.svg') }}')">
     <img src="{{ asset('assets/images/main-bg-mobile.svg') }}" alt="" class="image-main-mobile d-lg-none w-100">
     @if ($showBackButton)
         <x-landing.back-button class="back-button m-4" />
