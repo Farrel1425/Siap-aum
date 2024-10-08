300c7e7dbec5a42a934894df6c71f15f1a477139
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2024-10-08 18:47:31 +0800

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,7 +17,7 @@ window.Toast = Swal.mixin({
     toast: true,
     position: 'top-end',
     showConfirmButton: false,
-    timer: 6000,
+    timer: 10000,
     timerProgressBar: true,
     didOpen: (toast) => {
         toast.addEventListener('mouseenter', Swal.stopTimer)
