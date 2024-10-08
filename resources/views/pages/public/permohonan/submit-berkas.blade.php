3d6459272015135495f369bcfa3f0ac8d168d1cd
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2024-10-08 18:58:51 +0800

    feat: add form permohonan

--- resources/views/pages/public/permohonan/submit-berkas.blade.php
+++ resources/views/pages/public/permohonan/submit-berkas.blade.php
@@ -18,24 +18,38 @@
             <form action="{{ route('public.permohonan.submit-berkas', $permohonan->id) }}"
                   method="POST">
                 @csrf
+                <h5 class="mt-4">Form Permohonan</h5>
+                <div class="card">
+                    <div class="card-body">
+                        @foreach ($permohonan->formPermohonan as $form_permohonan)
+                            <x-dashboard.input-inline-text :readonly=True
+                                                           :required=True
+                                                           class="mb-3"
+                                                           id="{{ $form_permohonan->id }}"
+                                                           label="{{ $form_permohonan->label }}"
+                                                           name="{{ $form_permohonan->kode_isian }}"
+                                                           value="{{ $form_permohonan->value }}" />
+                        @endforeach
+                    </div>
+                </div>
                 <h5 class="mt-4">Upload Berkas Permohonan</h5>
                 @foreach ($permohonan->berkasPermohonan as $berkasPermohonan)
-                    <x-dashboard.input-inline-file-upload class="bg-white p-2 mx-1 mb-3 text-xsm"
-                                                   class_input="text-xsm"
-                                                   label="{{ $berkasPermohonan->nama }}"
-                                                   name="{{ $berkasPermohonan->id }}"
-                                                   uploadUrl="{{ route('public.permohonan.store-berkas', $permohonan->id) }}"
-                                                   downloadUrl="{{ $berkasPermohonan->filepath ? Storage::url($berkasPermohonan->filepath) : '' }}"
-                                                   :required="$berkasPermohonan->is_required" />
+                    <x-dashboard.input-inline-file-upload :required="$berkasPermohonan->is_required"
+                                                          class="bg-white p-2 mx-1 mb-3 text-xsm"
+                                                          class_input="text-xsm"
+                                                          downloadUrl="{{ $berkasPermohonan->filepath ? Storage::url($berkasPermohonan->filepath) : '' }}"
+                                                          label="{{ $berkasPermohonan->nama }}"
+                                                          name="{{ $berkasPermohonan->id }}"
+                                                          uploadUrl="{{ route('public.permohonan.store-berkas', $permohonan->id) }}" />
                 @endforeach
-                <button class="btn btn-primary bg-primary mt-3 w-100 d-block mb-4"
-                        type="submit" {{ !$is_all_uploaded? 'disabled' : '' }}>Simpan dan Ajukan Permohonan</button>
+                <button {{ !$is_all_uploaded ? 'disabled' : '' }}
+                        class="btn btn-primary bg-primary mt-3 w-100 d-block mb-4"
+                        type="submit">Simpan dan Ajukan Permohonan</button>
             </form>
         </section>
     </div>
 @endsection
 
 @push('scripts')
-    <script>
-    </script>
+    <script></script>
 @endpush
