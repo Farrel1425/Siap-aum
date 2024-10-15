d92e51cb7ea89909e49d6d2118a5fb7df6451754
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2024-10-15 10:54:27 +0800

    telesckope on dev env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if ($this->app->environment('development')) {
+            // enable telescope
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
