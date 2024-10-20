6db624608eb2d2194fd1fbbb72f6da18658a1006
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2024-10-21 01:48:21 +0800

    issue: Dashboard Verifikator

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Permohonan;
 use Illuminate\Http\Request;
+use App\Enums\StatusPermohonanEnum;
+use App\Http\Controllers\DashboardUserController;
 
 class DashboardController extends Controller
 {
@@ -14,7 +17,31 @@ class DashboardController extends Controller
             $dashboardUserController = new DashboardUserController();
             return $dashboardUserController->index($request);
         } else if (auth()->user()->is_verifikator) {
-            return view('pages.verifikator.dashboard.index');
+            $total_permohonan = Permohonan::whereNotIn('status', [
+                StatusPermohonanEnum::PENDING->value,
+                StatusPermohonanEnum::EXPIRED->value
+            ])->whereHas('alurPermohonan', function ($q) {
+                $q->where('verifikator_id', auth()->user()->id);
+            })->count();
+
+            $permohonan_diproses = Permohonan::whereNotIn('status', [
+                StatusPermohonanEnum::PENDING->value,
+                StatusPermohonanEnum::EXPIRED->value,
+                StatusPermohonanEnum::SELESAI->value,
+            ])->whereHas('alurPermohonan', function ($q) {
+                $q->where('verifikator_id', auth()->user()->id);
+            })->count();
+
+            $permohonan_selesai = Permohonan::where('status', StatusPermohonanEnum::SELESAI->value)
+                ->whereHas('alurPermohonan', function ($q) {
+                    $q->where('verifikator_id', auth()->user()->id);
+                })->count();
+
+            return view('pages.verifikator.dashboard.index', compact(
+                'total_permohonan',
+                'permohonan_diproses',
+                'permohonan_selesai'
+            ));
         } else {
             auth()->logout();
             return redirect()->route('login')->with('error', 'Anda tidak memiliki akses, silahkan login kembali');
--- resources/views/pages/verifikator/dashboard/index.blade.php
+++ resources/views/pages/verifikator/dashboard/index.blade.php
@@ -12,7 +12,7 @@
         </div>
         <section class="section">
             <div class="row">
-                <div class="col-12 col-lg-8">
+                <div class="col-12">
                     <div class="row">
                         <div class="col-4 col-md-4">
                             <div class="card">
@@ -24,7 +24,7 @@
                                             </div>
                                         </div>
                                         <div class="col-md-8 col-xxl-7 d-flex flex-column gap-2">
-                                            <h5 class="font-extrabold mb-0">112.000</h5>
+                                            <h5 class="font-extrabold mb-0">{{ number_format($total_permohonan) }}</h5>
                                             <h6 class="text-muted fw-normal">Usulan Masuk</h6>
                                         </div>
                                     </div>
@@ -41,8 +41,8 @@
                                             </div>
                                         </div>
                                         <div class="col-md-8 col-xxl-7 d-flex flex-column gap-2">
-                                            <h5 class="font-extrabold mb-0">112.000</h5>
-                                            <h6 class="text-muted fw-normal">Usulan Masuk</h6>
+                                            <h5 class="font-extrabold mb-0">{{ number_format($permohonan_diproses) }}</h5>
+                                            <h6 class="text-muted fw-normal">Usulan Diproses</h6>
                                         </div>
                                     </div>
                                 </div>
@@ -58,19 +58,38 @@
                                             </div>
                                         </div>
                                         <div class="col-md-8 col-xxl-7 d-flex flex-column gap-2">
-                                            <h5 class="font-extrabold mb-0">112.000</h5>
-                                            <h6 class="text-muted fw-normal">Usulan Masuk</h6>
+                                            <h5 class="font-extrabold mb-0">{{ number_format($permohonan_selesai) }}</h5>
+                                            <h6 class="text-muted fw-normal">Usulan Selesai</h6>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-                <div class="col-12 col-md-4">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="d-inline">Histori Aktivitas</h3>
+                        <div class="card">
+                            <div class="card-header">
+                                <h4 class="card-title
+                                             mb-0">Usulan Terbaru</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-sm"
+                                           id="permohonan-table">
+                                        <thead>
+                                            <tr>
+                                                <th>No</th>
+                                                <th>Jenis Permohonan</th>
+                                                <th>Nomor Registrasi</th>
+                                                <th>Tanggal Pengajuan</th>
+                                                <th>Nama Pemohon</th>
+                                                <th>Permohonan Rekomendasi</th>
+                                                <th>Surat Rekomendasi</th>
+                                                <th>Status</th>
+                                                <th class="text-center">Aksi</th>
+                                            </tr>
+                                        </thead>
+                                    </table>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -80,5 +99,87 @@
 @endsection
 
 @push('scripts')
-    <script></script>
+    <script>
+        $(document).ready(function() {
+            $('#permohonan-table').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: '{{ route('verifikator.permohonan.table') }}',
+                    data: function(d) {
+                        d.search = $('#dt-search-0').val();
+                        d.status = $('#status').val();
+                        d.jenis_izin_id = $('#jenis_izin_id').val();
+                    }
+                },
+                order: [
+                    [3, 'desc']
+                ],
+                columns: [{
+                        data: null,
+                        name: 'index',
+                        searchable: false,
+                        orderable: false,
+                        className: 'text-center',
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    },
+                    {
+                        data: 'nama_jenis_izin',
+                        name: 'nama_jenis_izin',
+                        orderable: false
+                    },
+                    {
+                        data: 'nomor_registrasi',
+                        name: 'nomor_registrasi',
+                        orderable: false
+                    },
+                    {
+                        data: 'waktu_pengajuan',
+                        name: 'waktu_pengajuan'
+                    },
+                    {
+                        data: 'nama_pemohon',
+                        name: 'nama_pemohon',
+                        orderable: false
+                    },
+                    {
+                        data: 'surat_permohonan_rekomendasi',
+                        name: 'surat_permohonan_rekomendasi',
+                        className: 'text-center',
+                        render: function(data, type, row) {
+                            return data ?
+                                `<span class='text-success'><i class='isax-bold isax-tick-circle'></i></span>` :
+                                '-';
+                        },
+                        orderable: false
+                    },
+                    {
+                        data: 'surat_rekomendasi',
+                        name: 'surat_rekomendasi',
+                        className: 'text-center',
+                        render: function(data, type, row) {
+                            return data ?
+                                `<span class='text-success'><i class='isax-bold isax-tick-circle'></i></span>` :
+                                '-';
+                        },
+                        orderable: false
+                    },
+                    {
+                        data: 'status_badge',
+                        name: 'status_badge',
+                        className: 'text-center',
+                        orderable: false
+                    },
+                    {
+                        data: 'action',
+                        name: 'action',
+                        className: 'text-center',
+                        orderable: false
+                    }
+                ],
+            });
+        });
+    </script>
 @endpush
