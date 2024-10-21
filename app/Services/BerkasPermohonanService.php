70753e7187fe2d521dc64c933b7cc572d545976f
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2024-10-21 17:03:42 +0800

    issue gabisa upload berkas ketika sudah selesai

--- app/Services/BerkasPermohonanService.php
+++ app/Services/BerkasPermohonanService.php
@@ -183,7 +183,7 @@ class BerkasPermohonanService
     public function isAllBerkasValidFromVerifikator(AlurPermohonan $alurPermohonan)
     {
         $alurPermohonan->relationLoaded('permohonan') || $alurPermohonan->load('permohonan');
-        if($alur_permohonan->is_done) {
+        if($alurPermohonan->is_done) {
             return true;
         }
         $alurPermohonan->permohonan->relationLoaded('berkasPermohonan') || $alurPermohonan->permohonan->load(['berkasPermohonan' => function ($query) use ($alurPermohonan) {
