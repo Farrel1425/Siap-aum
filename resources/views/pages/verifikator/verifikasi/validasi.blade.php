1864cb10344be6b4a4271de662a780dcc72409a3
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2024-11-14 05:34:49 +0800

    fix: Ubah menjadi kata Data Berkas Penunjang

--- resources/views/pages/verifikator/verifikasi/validasi.blade.php
+++ resources/views/pages/verifikator/verifikasi/validasi.blade.php
@@ -185,7 +185,7 @@
                         @endif
                     </div>
                 @endforeach
-                <h5 class="mt-4">Data Berkas Verifikator</h5>
+                <h5 class="mt-4">Data Berkas Penunjang</h5>
                 @if (
                     $alur_permohonan->jenis_verifikator == App\Enums\JenisVerifikatorEnum::FO->value &&
                         $permohonan->status != App\Enums\StatusPermohonanEnum::SELESAI->value)
