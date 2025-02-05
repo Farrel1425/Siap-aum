75808d8ce6a3a8db20b0930b79ff8361825af90c
Aang Pangantyas Sampurna <aangpangantyas@gmail.com>
2025-02-05 15:05:37 +0800

    feat: test

--- resources/views/mail/auth/otp-register.blade.php
+++ resources/views/mail/auth/otp-register.blade.php
@@ -1,14 +1,12 @@
 <x-mail::message>
-# Verifikasi Email
+<h2 style="text-align: center;color:#181818;font-weight: bold;font-size:22px;margin: 10px 0;">Verifikasi Email</h2>
 
-Berikut adalah kode OTP untuk verifikasi email Anda. Jangan berikan kode ini kepada siapapun.
-
+<p style="text-align: start;color:#181818;margin: 10px;">Berikut adalah kode OTP untuk verifikasi email Anda. Jangan berikan kode ini kepada siapapun.</p>
 <x-mail::panel>
-<strong>{{ $otp }}</strong>
+    <strong>{{ $otp }}</strong>
 </x-mail::panel>
 
-Jika Anda tidak merasa melakukan tindakan ini, abaikan email ini.<br>
-
+<p style="text-align: start;color:#181818;margin: 10px;">Jika Anda tidak merasa melakukan tindakan ini, abaikan email ini</p>
 Terima kasih,<br>
 <strong>{{ config('app.name') }} - E Perizinan Kabupaten Buleleng</strong>
 </x-mail::message>
